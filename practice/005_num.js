0b142d17f25552e7c9d5a7b274884fb162460393
b3lon9 <119420119+b3lon9@users.noreply.github.com>
2022-12-09 15:47:08 +0900

    [js] 기본구조
    연산

---
+++ practice/005_num.js
@@ -0,0 +1,13 @@
+var num1, num2;
+var result1;
+var result2;
+var result3;
+var result4;
+
+num1 = 20;
+num2 = 5;
+
+result1 = num1 + num2;
+result2 = num1 - num2;
+result3 = num1 * num2;
+result4 = num1 / num2;
---
+++ practice/006_str.js
@@ -0,0 +1,6 @@
+var str = "연결되나?";
+var str2 = "연결 연산자";
+
+var result = str + str2;
+
+var result2 = str * 30;
